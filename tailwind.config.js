8fcabfe466fed4e3019641e60dba54138ee31660
Parvez <miazyparvez@gmail.com>
2024-01-06 14:18:28 +0600

    theme configuration change and font add

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
+const defaultTheme = require ("tailwindcss/defaultTheme");
 module.exports = {
   mode:'jit',
   purge:["./public/**/*.{html,js}"],
+  darkMode:"media",
   theme: {
+    screens:{
+sm:'540px',
+lg:'1000px',
+"4k":'2560px'
+    },
+    fontFamily:{
+    'sans':['Roboto',...defaultTheme.fontFamily.sans]
+    },
     extend: {},
   },
   varients:{
